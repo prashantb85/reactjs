3f40dbc87b2de29b5fee0590d62ca01db125e3f9
prashantb85 <62536398+prashantb85@users.noreply.github.com>
2023-05-15 08:33:25 +0530

    Create App.cs

---
+++ source/App.cs
@@ -0,0 +1,21 @@
+.component
+{
+  text-align: left;
+  margin: 300px;
+  width: 300px;
+}
+
+.intellisense
+{
+  border: 1px solid black;
+}
+.intellisenseItems
+{
+  cursor:pointer;
+  text-align: start;
+  margin: 2px 0 0 0 ;
+}
+
+input{
+  width: 77%;
+}
